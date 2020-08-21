de8bedfd727b8e3f6b2720a4080b2a4e0c023eb2
Heewon <ccc4804@naver.com>
2020-08-22 03:43:16 +0900

    Chapter4 - react-async로 요청 상태 관리하기

--- README.md
+++ README.md
@@ -2,5 +2,10 @@ axios
 	- API 요청을 할 때 promise 기반으로 요청을 하는 라이브러리
 	- 해당 챕터에서는 https://jsonplaceholder.typicode.com/ 제공하는 API를 사용한다.
 	
+	- 설치 명령어 : yarn add axios
+	
 	useReducer : 사용하는 이유는 요청 관리에 대한 로직을 따로 분리해서 재사용이 가능하다.
+
+react-async
+	- 설치 명령어 : yarn add react-async
 	
--- api-integrate/src/Users.js
+++ api-integrate/src/Users.js
@@ -1,6 +1,6 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import useAsync from "./useAsync";
+import { useAsync } from "react-async";
 import User from "./User";
 
 async function getUsers() {
@@ -11,15 +11,14 @@ async function getUsers() {
 }
 
 function Users() {
-  // 컴포넌트가 처음 랜더링 될때 요청을 생략하게 true로 설정
-  const [state, refetch] = useAsync(getUsers, [], true);
-  const [userId, setUserId] = useState(null)
+  const [userId, setUserId] = useState(null);
+  const { data: users, error, isLoading, reload,run } = useAsync({
+    deferFn: getUsers,
+  });
 
-  //data의 변수명을 users로 변경한다.
-  const { loading, data: users, error } = state;
-  if (loading) return <div>로딩 중...</div>;
+  if (isLoading) return <div>로딩 중...</div>;
   if (error) return <div>에러가 발생했습니다.</div>;
-  if (!users) return <button onClick={refetch}>불러오기</button>;
+  if (!users) return <button onClick={run}>불러오기</button>;
 
   return (
     <>
@@ -30,7 +29,7 @@ function Users() {
           </li>
         ))}
       </ul>
-      <button onClick={refetch}>다시 불러오기</button>
+      <button onClick={reload}>다시 불러오기</button>
       {userId && <User id={userId} />}
     </>
   );
